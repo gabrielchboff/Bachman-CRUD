cb43f9961ca48c78b80fad2c63131f12bb9ea2f0
unknown <gabrielchagasboff@gmail.com>
2022-11-10 10:26:12 -0300

    crud completed

--- src/main.go
+++ src/main.go
@@ -18,42 +18,92 @@ type Movie struct {
 }
 
 type Director struct {
-	First    string `json:"firstname"`
-	Lastname string `jason:"lastname"`
+	Firstname string `json:"firstname"`
+	Lastname  string `jason:"lastname"`
 }
 
 var movies []Movie
 
-
-func getMovies(w http.ResponseWriter, r http.Request)  {
-					
+func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	json.NewEncoder(w).Encode(movies)
 
 }
 
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	parms := mux.Vars(r)
+	var movie Movie
+	for index, iten := range movies {
+		if iten.ID == parms["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = strconv.Itoa(rand.Intn(100000000))
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+
+		}
+	}
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	parms := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == parms["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	parms := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == parms["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+}
 
 func main() {
 	r := mux.NewRouter()
-  
-  movies = append(movies, Movie{
-  	ID:"1", 
-  	Isbn:"438227", 
-  	Title:"Movie One", 
-  	Director: &Director{
-  		Firstname: "Jhon", Lastname: "Doe"
-  	},
-  } )
-  movies = append(movies, Movie{
-  	ID:"2", 
-  	Isbn: "73565", 
-  	Title: "Movie Two",
-  	Director: &Director{
-  		First: "Steve", 
-  		Lastname:"Smith"
-  	},
-  } )
-  
+
+	movies = append(movies,
+		Movie{
+			ID:    "1",
+			Isbn:  "438227",
+			Title: "Movie One",
+			Director: &Director{
+				Firstname: "Jhon",
+				Lastname:  "Doe",
+			},
+		})
+	movies = append(movies, Movie{
+		ID:    "2",
+		Isbn:  "73565",
+		Title: "Movie Two",
+		Director: &Director{
+			Firstname: "Steve",
+			Lastname:  "Smith",
+		},
+	})
+
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
